c239de4d7e333ef7e18aeeda341899d1989b18b3
Daniel DR <danieldr-@hotmail.com>
2024-05-29 17:53:52 -0300

    header
    adcionado header

---
+++ pdani-001/src/Componentes/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header className="bg-gray-600 
+                           font-bold 
+                           p-7 
+                           text-gray-100
+                           text-center
+                           text-4xl
+                           rounded" 
+                           >Comando para Descrição de Item</header>
+    </div>
+  )
+}
